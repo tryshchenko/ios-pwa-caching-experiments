4a4ec2234094d1604d92955cccd237de0e786cbc
Oleksandr Tryshchenko <ensaierwa@gmail.com>
2019-04-06 11:56:37 +0200

    Basic cache

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,14 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js');
 
 if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
+    workbox.precaching.precacheAndRoute(
+        [
+          { url: '/index.html', revision: '1' },
+        ],
+        {
+          directoryIndex: null,
+        }
+    );
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
